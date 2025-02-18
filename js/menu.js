0c37651160468a9a7bbfb91ef48f17c1a2cee0c8
venia <bulyginveniamin@gmail.com>
2025-02-18 15:50:31 +0200

    commit menu

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,43 @@
-import { Game } from './game.js';
+  import { Game } from './game.js';
 
-export function initializeMenu() {
-  const menuContainer = document.getElementById('menu-container');
-  const canvas = document.getElementById('game-canvas');
-  const joystickContainer = document.getElementById('joystick-container');
-  const circles = document.querySelectorAll('.character-circle');
+  export function initializeMenu() {
+    const menuContainer = document.getElementById('menu-container');
+    const canvas = document.getElementById('game-canvas');
+    const joystickContainer = document.getElementById('joystick-container');
+    const circles = document.querySelectorAll('.character-circle');
 
-  circles.forEach((circle) => {
-    circle.addEventListener('click', () => {
-      const color = circle.dataset.color;
-      menuContainer.style.display = 'none';
-      canvas.style.display = 'block'; // Показываем канвас
-      joystickContainer.style.display = 'block'; // Показываем джойстик
-      new Game(color);
+    circles.forEach((circle) => {
+      circle.addEventListener('click', () => {
+        const color = circle.dataset.color;
+        menuContainer.style.display = 'none';
+        canvas.style.display = 'block'; // Показываем канвас
+        joystickContainer.style.display = 'block'; // Показываем джойстик
+        new Game(color);
+      });
     });
-  });
-}
+  }
+
+
+  function changeImage() {
+    var img = document.querySelector(".smallMenuButton img"); 
+    if (img.src.includes("/images/soundOn4.png")) {
+        img.src = "/images/mute2.png"; 
+    } else {
+        img.src = "/images/soundOn4.png"; 
+    }
+  }
+
+  function choosePlayers(){
+    var popup = document.querySelector(".choosePlayers")
+    if(popup.style.display === "none"){
+        popup.style.display = "block"
+    }
+    else{
+        popup.style.display = "none"
+    }
+  }
+
+  function arrowButton(){
+    var arrow = document.querySelector(".choosePlayers")
+    arrow.style.display  = "none"
+  }
